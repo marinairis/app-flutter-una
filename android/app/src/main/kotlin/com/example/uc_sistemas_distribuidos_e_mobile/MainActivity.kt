29ecbebc5e925d927b5aaee799b43a27ada95b86
Marina Iris <marinairis11@gmail.com>
2021-09-29 19:55:33 -0300

    adiciona primeiras atividades

---
+++ android/app/src/main/kotlin/com/example/uc_sistemas_distribuidos_e_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uc_sistemas_distribuidos_e_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pratica02/pratica.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Exercíco Prática 02
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Marina Íris! =)',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.blue
+        ),
+      ),
+    ),
+  );
+}
