97d9327cbec723fd1fe2dab11c8ab6289f1da4e2
Vitor Leone <vitor-leone@hotmail.com>
2021-10-24 13:57:01 -0300

    adicionando pratica07

---
+++ lib/pratica07/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Container(
+        child: Image(
+          image: NetworkImage(
+            'https://picsum.photos/250?image=37',
+          ),
+        ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.yellow,
+        ),
+        padding: EdgeInsets.all(100),
+        margin: const EdgeInsets.all(25),
+      ),
+    ),
+  );
+}
