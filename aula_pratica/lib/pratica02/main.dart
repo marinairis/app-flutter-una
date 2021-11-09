fbe4ab5d7213b1f5f438e94dc8f1f798aaacfc84
Marina Iris <73672906+marinairis@users.noreply.github.com>
2021-11-09 20:22:01 -0300

    rebase

--- aula_pratica/lib/pratica02/main.dart
+++ aula_pratica/lib/pratica02/main.dart
@@ -1,8 +1,4 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-
-// Exercíco Prática 02
 
 void main() {
   runApp(
@@ -10,9 +6,7 @@ void main() {
       child: Text(
         'Marina Íris! =)',
         textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.blue
-        ),
+        style: TextStyle(color: Colors.blue),
       ),
     ),
   );
