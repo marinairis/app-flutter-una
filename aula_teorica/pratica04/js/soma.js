9ae244951eb62a06ecf061393eaad9fa17a2c842
Marina Iris <73672906+marinairis@users.noreply.github.com>
2021-11-18 20:54:01 -0300

    atividade 4 de htnml

--- aula_teorica/pratica04/js/soma.js
+++ aula_teorica/pratica04/js/soma.js
@@ -1,13 +1,6 @@
-function somar(a, b) {
-  var num_um = a;
-  var num_dois = b;
-  var total = num_um + num_dois;
-  document.write("Valor total da soma: " + total);
-}
-
 function somarValores(){
-  var s1 = document.getElementById("txt1").value;
-  var s2 = document.getElementById("txt2").value;
-  var s3 = s1 + s2;
-  alert(s3);
+  var n1 = document.getElementById("txt1").value;
+  var n2 = document.getElementById("txt2").value;
+  var soma = parseInt(n1) + parseInt(n2);
+  document.getElementById("result").innerHTML = soma;
   }
