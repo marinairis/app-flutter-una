f29452cf6e6eebe45deac20ea2680290460ea316
marinairis <marinairis11@gmail.com>
2021-11-19 08:50:24 -0300

    primeiras modificaçoes form

--- aula_teorica/pratica04ex02/js/cadastro.js
+++ aula_teorica/pratica04ex02/js/cadastro.js
@@ -1,63 +1,71 @@
 function mascData(objeto, evento) {
-    //6 braces 46 delete
-    if (evento.keyCode == 8 || evento.keyCode == 46) {
-        return true;
-    }
-
-    //48 e 47: 0,1,2,3,4,5,6,7,8 e 9
-    if (evento.keyCode < 48 || evento.keyCode > 57) {
-        alert("Somente números");
-        return false;
-    }
-
-    if (objeto.value.length == 10) {
-        return false;
-    }
-
-    if ((objeto.value.length == 2) || (objeto.value.length == 5)) {
-        objeto.value = objeto.value + '/';
-    }
-
+  //6 braces 46 delete
+  if (evento.keyCode == 8 || evento.keyCode == 46) {
     return true;
+  }
+
+  //48 e 47: 0,1,2,3,4,5,6,7,8 e 9
+  if (evento.keyCode < 48 || evento.keyCode > 57) {
+    alert("Somente números");
+    return false;
+  }
+
+  if (objeto.value.length == 10) {
+    return false;
+  }
+
+  if ((objeto.value.length == 2) || (objeto.value.length == 5)) {
+    objeto.value = objeto.value + '/';
+  }
+
+  return true;
 }
 
+var form = document.getElementById("form")
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log(event);
+});
+
 function verificarFormulario() {
-    var mensagemErro = "";
+  var mensagemErro = "";
 
-    if (nome.value == "") {
-        mensagemErro += "Informe o nome.<br>";
-    }
+  if (nome.value == "") {
+    console.log(nome);
+    mensagemErro += "Informe o nome.<br>";
+  }
 
-    if (email.value == "") {
-        mensagemErro += "Informe o email.<br>";
-    }
+  if (email.value == "") {
+    mensagemErro += "Informe o email.<br>";
+  }
 
-    if (datanasc.value == "") {
-        mensagemErro += "Informe a data de nascimento.<br>";
-    }
+  if (datanasc.value == "") {
+    mensagemErro += "Informe a data de nascimento.<br>";
+  }
 
-    if (escolaridade.value == "") {
-        mensagemErro += "Informe a escolaridade.<br>";
-    }
+  if (escolaridade.value == "") {
+    mensagemErro += "Informe a escolaridade.<br>";
+  }
 
-    var sexoPreenchido = false;
-    var opcoesSexo = document.getElementByName("sexo");
-    for (var i = 0; i < opcoesSexo.length; i++) {
-        if (opcoesSexo[i].checked) {
-            sexoPreenchido = true;
-            break;
-        }
+  var sexoPreenchido = false;
+  var opcoesSexo = document.getElementByName("sexo");
+  for (var i = 0; i < opcoesSexo.length; i++) {
+    if (opcoesSexo[i].checked) {
+      sexoPreenchido = true;
+      break;
     }
+  }
 
-    if (sexoPreenchido == false) {
-        mensagemErro += "Informe o sexo.<br>";
-    }
+  if (sexoPreenchido == false) {
+    mensagemErro += "Informe o sexo.<br>";
+  }
 
-    if (mensagemErro == "") {
-        alert("Funcionário cadastrado com sucesso!!!");
-        return true;
-    } else {
-        resp.innerHTML = mensagemErro;
-        return false;
-    }
+  if (mensagemErro == "") {
+    alert("Funcionário cadastrado com sucesso!!!");
+    return true;
+  } else {
+    resp.innerHTML = mensagemErro;
+    return false;
+  }
 }
